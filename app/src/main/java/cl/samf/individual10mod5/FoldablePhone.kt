f4473181d05ddbb6518bc1a44e344394f0abf898
Sergio <sergiomolinaflores1@gmail.com>
2023-07-12 22:06:14 -0400

    Ejercicio 10 Mod 5, Ok incluido funcionamiento

--- app/src/main/java/cl/samf/individual10mod5/FoldablePhone.kt
+++ app/src/main/java/cl/samf/individual10mod5/FoldablePhone.kt
@@ -4,15 +4,22 @@ class FoldablePhone(isScreenLightOn: Boolean = false) : Phone(isScreenLightOn) {
     var isFoldable: Boolean = false
 
     override fun switchOn() {
-       if (!isFoldable){
-           isScreenLightOn = true
-       }
-        fun abrir() {
+        if (!isFoldable) {
+            isScreenLightOn = true
+        }
+    }
+        fun abrir () {
             isFoldable = true
         }
-        fun cerrar(){
+        fun cerrar() {
             isFoldable = true
         }
     }
+
+fun main (){
+    val foldable = FoldablePhone()
+    println(foldable.isFoldable)
+    foldable.abrir()
+    println(foldable.isFoldable)
 }
 
